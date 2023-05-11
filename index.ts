422a0ee205a0d2497aae04d2fbb489cd060f4770
ben lim <lim6112j@gmail.com>
2023-05-11 16:19:53 +0900

    start to all scrapping

--- index.ts
+++ index.ts
@@ -8,18 +8,35 @@ import puppeteer from 'puppeteer'
   await page.$eval('#submenu1 > ul > li:nth-child(4) > ul > li:nth-child(1) > a', elem => elem.click())
   await page.waitForTimeout(1000)
   await page.focus('#searchODStartSpaceNm')
-  await page.keyboard.type('아산시')
+  const searchStr = '안산시'
+  console.log("searching ... ", searchStr)
+  await page.keyboard.type(searchStr)
   const search_btn = await page.waitForSelector('#space6 > li.box_flex > button') // 검색 클릭
   await search_btn?.click()
-  await page.waitForTimeout(1000)
+  console.log("start location searching button clicked...")
+  // 출발지  input box click
   await page.evaluate(() => {
-    (document.querySelectorAll('label[id^="label_"] > div')[0] as HTMLElement).click()
+    const startInputBox = document.querySelectorAll('#selODStartArea_1')[0] as HTMLInputElement
+    startInputBox ? startInputBox.click() : console.log("구분이 잘못 지정된 지역명입니다")
   })
-  // console.log(label)
-  // await page.click('#label_44200 > div')
+
+  // 도착지 전체선택
+  await page.evaluate(() => {
+    const EndInputBox = document.querySelectorAll('#rdAlocSelN')[0] as HTMLInputElement // 전체 선택
+    EndInputBox ? EndInputBox.click() : console.log("구분이 잘못 지정된 지역명입니다")
+  })
+
+  // 결과 검색
+  const result_btn = await page.waitForSelector('#btnSearch > button') // 검색 클릭
+  await result_btn?.click()
+  await page.waitForSelector('#tab1 > div.pivotResult > div.but_area_back > button')
+  await page.waitForSelector('#rgrstyReportResult > table')
+
+
+  // screenshot
   await page.waitForTimeout(1000)
   await page.screenshot({ path: "./stcis.png", fullPage: true })
-  await page.waitForTimeout(100000)
+  console.log("browser closing ...")
   await browser.close()
 }
 )()
