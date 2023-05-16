d60f36e8491017e91f80aa0abb5d6bd90bc5b6ef
ben lim <lim6112j@gmail.com>
2023-05-16 19:15:44 +0900

    synch multi get made

--- index.ts
+++ index.ts
@@ -1,15 +1,28 @@
+import { Cluster } from 'puppeteer-cluster'
 import ssmd from './singleStartMultiDestination'
 import mssd from './multiStartSingleDestination'
 (async () => {
 
-  try {
-    if (process.argv[2] === undefined) { throw new Error("argument needed") }
-  } catch (e) {
-    console.log('argument needed')
-    process.exit(1)
-  }
-  await ssmd()
-  //await mssd()
-  // 출발지 검색 창에 입력
+  // try {
+  //   if (process.argv[2] === undefined) { throw new Error("argument needed") }
+  // } catch (e) {
+  //   console.log('argument needed')
+  //   process.exit(1)
+  // }
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_BROWSER,
+    maxConcurrency: 1,
+    puppeteerOptions: {
+      headless: false
+    }
+  });
+  await cluster.task(async ({ page, data: searchStr }) => {
+    await ssmd(page, searchStr)
+  })
+  cluster.queue('아산시')
+  cluster.queue('경주시')
+  cluster.queue('안산시')
+  await cluster.idle()
+  await cluster.close()
 }
 )()
--- singleStartMultiDestination.ts
+++ singleStartMultiDestination.ts
@@ -1,47 +1,44 @@
-import puppeteer from 'puppeteer'
-export default async function() {
-  const browser = await puppeteer.launch({ headless: false, userDataDir: './data/' })
-  const page = await browser.newPage()
+import { Page } from 'puppeteer'
+const TIMEOUT = 15000
+export default async function(page: Page, searchStr: string) {
   await page.goto('http://stcis.go.kr')
-  await page.waitForSelector('body > header > div.header_inner > div.gnb > ul > li:nth-child(1) > a', { timeout: 1000 })
+  await page.waitForSelector('body > header > div.header_inner > div.gnb > ul > li:nth-child(1) > a', { timeout: TIMEOUT })
   await page.focus('body > header > div.header_inner > div.gnb > ul > li:nth-child(1) > a')
   // main 페이지에서 메뉴 hover 시 서브메뉴 펼처짐(일반버스 도시철도 이용 O/D)
-  await page.waitForSelector('#submenu1 > ul > li:nth-child(4) > ul > li:nth-child(1) > a', { timeout: 1000 })
+  await page.waitForSelector('#submenu1 > ul > li:nth-child(4) > ul > li:nth-child(1) > a', { timeout: TIMEOUT })
   await page.$eval('#submenu1 > ul > li:nth-child(4) > ul > li:nth-child(1) > a', elem => elem.click())
 
   // 출발지 선택
-  const startInputBox = await page.waitForSelector('#rdStgptSelY', { timeout: 1000 })
+  const startInputBox = await page.waitForSelector('#rdStgptSelY', { timeout: TIMEOUT })
   startInputBox?.click()
 
-  await page.waitForSelector('#searchODStartSpaceNm', { timeout: 1000 })
+  await page.waitForSelector('#searchODStartSpaceNm', { timeout: TIMEOUT })
   await page.focus('#searchODStartSpaceNm')
-  const searchStr = process.argv[2]
   console.log("searching ... ", searchStr)
-  await page.keyboard.type(searchStr, { delay: 200 })
-  const search_btn = await page.waitForSelector('#space6 > li.box_flex > button', { timeout: 1000 }) // 검색 클릭
-  search_btn?.click()
+  await page.keyboard.type(searchStr, { delay: 300 })
+  const search_btn = await page.waitForSelector('#space6 > li.box_flex > button', { timeout: TIMEOUT }) // 검색 클릭
+  await search_btn?.click()
   console.log("start location searching button clicked...")
   // 출발지  input box click
-  const startInputCheck = await page.waitForSelector('#selODStartArea_1', { timeout: 10000 })
-  startInputCheck?.click()
+  await page.waitForTimeout(1000)
+  const startInputCheck = await page.waitForSelector('#divODStart > ol > li', { timeout: TIMEOUT })
+  await startInputCheck?.click()
   await page.waitForTimeout(1000)
   // 도착지 전체선택
-  const endInputBox = await page.waitForSelector('#rdAlocSelN', { timeout: 10000 })
+  const endInputBox = await page.waitForSelector('#rdAlocSelN', { timeout: TIMEOUT })
   endInputBox?.click()
 
   // 결과 검색
-  const result_btn = await page.waitForSelector('#btnSearch > button', { timeout: 1000 }) // 검색 클릭
+  const result_btn = await page.waitForSelector('#btnSearch > button', { timeout: TIMEOUT }) // 검색 클릭
   result_btn?.click()
   //download
-  await page.waitForSelector('#none', { timeout: 1000 })
-  const domBtn = await page.waitForSelector('#rgrstyReportResult > h2 > p > span:nth-child(1)', { timeout: 10000 })
+  const hiddenlayer = await page.waitForSelector('#none', { timeout: TIMEOUT })
+  const domBtn = await page.waitForSelector('#rgrstyReportResult > h2 > p > span:nth-child(1)', { timeout: TIMEOUT })
   domBtn?.click()
   // screenshot
-  const backBtn = await page.waitForSelector('#tab1 > div.pivotResult > div.but_area_back > button', { timeout: 10000 })
+  const backBtn = await page.waitForSelector('#tab1 > div.pivotResult > div.but_area_back > button', { timeout: TIMEOUT })
   backBtn?.click()
-
-  await page.waitForTimeout(100000)
+  hiddenlayer?.getProperty('style').then(val => console.log(val))
+  await page.waitForTimeout(50000)
   // await page.screenshot({ path: "./stcis.png", fullPage: true })
-  console.log("browser closing ...")
-  await browser.close()
 } 
