908c480ffebb7cd17f3fac45346877977158a939
ben lim <lim6112j@gmail.com>
2023-05-16 16:12:08 +0900

    출도착 전체검색 완료

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import puppeteer from 'puppeteer'
+import ssmd from './singleStartMultiDestination'
+import mssd from './multiStartSingleDestination'
 (async () => {
 
   try {
@@ -7,89 +8,8 @@ import puppeteer from 'puppeteer'
     console.log('argument needed')
     process.exit(1)
   }
-  async function move_page(idx: number) {
-    // #rgrstyReportResult > div > ul > li:nth-child(2) > a
-    const pagination = await page.waitForSelector(`#rgrstyReportResult > div > ul > li:nth-child(${idx}) > a`)
-    await pagination?.click()
-  }
-  async function get_table_value() {
-    const page_data = await page.evaluate(() => Array.from(
-      document.querySelectorAll('#rgrstyReportResult > table > tbody > tr'),
-      (row: any) => Array.from(row.querySelectorAll('th, td'), (cell: HTMLElement) => cell.innerText)
-    ))
-    return page_data
-  }
-  async function wait_for_pagination() {
-
-    await page.waitForSelector('#tab1 > div.pivotResult > div.but_area_back > button')
-    await page.waitForSelector('#rgrstyReportResult > table')
-  }
-  const browser = await puppeteer.launch({ headless: false, userDataDir: './data/' })
-  const page = await browser.newPage()
-  await page.goto('http://stcis.go.kr')
-  await page.waitForSelector('body > header > div.header_inner > div.gnb > ul > li:nth-child(1) > a', { timeout: 1000 })
-  await page.focus('body > header > div.header_inner > div.gnb > ul > li:nth-child(1) > a')
-  // main 페이지에서 메뉴 hover 시 서브메뉴 펼처짐(일반버스 도시철도 이용 O/D)
-  await page.waitForSelector('#submenu1 > ul > li:nth-child(4) > ul > li:nth-child(1) > a', { timeout: 1000 })
-  await page.$eval('#submenu1 > ul > li:nth-child(4) > ul > li:nth-child(1) > a', elem => elem.click())
-
+  await ssmd()
+  //await mssd()
   // 출발지 검색 창에 입력
-  await page.waitForSelector('#searchODStartSpaceNm', { timeout: 1000 })
-  await page.focus('#searchODStartSpaceNm')
-  const searchStr = process.argv[2]
-  console.log("searching ... ", searchStr)
-  await page.keyboard.type(searchStr, { delay: 100 })
-  const search_btn = await page.waitForSelector('#space6 > li.box_flex > button', { timeout: 1000 }) // 검색 클릭
-  await search_btn?.click()
-  console.log("start location searching button clicked...")
-  // 출발지  input box click
-  await page.evaluate(() => {
-    const startInputBox = document.querySelectorAll('#selODStartArea_1')[0] as HTMLInputElement
-    startInputBox ? startInputBox.click() : console.log("구분이 잘못 지정된 지역명입니다")
-  })
-
-  // 도착지 전체선택
-  await page.evaluate(() => {
-    const EndInputBox = document.querySelectorAll('#rdAlocSelN')[0] as HTMLInputElement // 전체 선택
-    EndInputBox ? EndInputBox.click() : console.log("구분이 잘못 지정된 지역명입니다")
-  })
-
-  // 결과 검색
-  const result_btn = await page.waitForSelector('#btnSearch > button') // 검색 클릭
-  await result_btn?.click()
-  await wait_for_pagination()
-  // table paging
-  //
-  const pageNum = await page.evaluate(
-    () => {
-      return document.querySelectorAll('.tbl_paging > ul > li').length
-    }
-  )
-  console.log("pagenation number = ", pageNum)
-  // table to json
-  // #rgrstyReportResult > div > ul > li:nth-child(2) > a
-  let data: any = [];
-  for (let index = 0; index < pageNum; index++) {
-    if (index === 0) {
-      const _data = await get_table_value()
-      data = data.concat(_data)
-    } else {
-      await move_page(index + 1)
-      await wait_for_pagination()
-      const _data = await get_table_value()
-      data = data.concat(_data)
-    }
-  }
-  console.log(data)
-  const loadingDiv = await page.waitForSelector('#none', { timeout: 1000 })
-  const domBtn = await page.waitForSelector('#rgrstyReportResult > h2 > p > span:nth-child(1)', { timeout: 1000 })
-  await domBtn?.click()
-  // screenshot
-  const backBtn = await page.waitForSelector('#tab1 > div.pivotResult > div.but_area_back > button', { timeout: 10000 })
-  await backBtn?.click()
-  await page.waitForTimeout(1000000)
-  // await page.screenshot({ path: "./stcis.png", fullPage: true })
-  console.log("browser closing ...")
-  await browser.close()
 }
 )()
--- multiStartSingleDestination.ts
+++ multiStartSingleDestination.ts
@@ -1,3 +1,4 @@
+
 import puppeteer from 'puppeteer'
 export default async function() {
   const browser = await puppeteer.launch({ headless: false, userDataDir: './data/' })
@@ -8,40 +9,41 @@ export default async function() {
   // main 페이지에서 메뉴 hover 시 서브메뉴 펼처짐(일반버스 도시철도 이용 O/D)
   await page.waitForSelector('#submenu1 > ul > li:nth-child(4) > ul > li:nth-child(1) > a', { timeout: 1000 })
   await page.$eval('#submenu1 > ul > li:nth-child(4) > ul > li:nth-child(1) > a', elem => elem.click())
-
-  // 출발지 선택
-  const startInputBox = await page.waitForSelector('#rdStgptSelY', { timeout: 1000 })
+  // 시군구 - 선택 - 검색어 입력
+  // 출발지 - 전체 선택
+  const startInputBox = await page.waitForSelector('#rdStgptSelN', { timeout: 1000 })
   startInputBox?.click()
-
-  await page.waitForSelector('#searchODStartSpaceNm', { timeout: 1000 })
-  await page.focus('#searchODStartSpaceNm')
+  await page.waitForTimeout(1000)
+  // 도착지 선택
+  const endInputBox = await page.waitForSelector('#rdAlocSelY', { timeout: 1000 })
+  endInputBox?.click()
+  // 검색어 입력
+  await page.waitForSelector('#searchODEndSpaceNm', { timeout: 1000 })
+  await page.focus('#searchODEndSpaceNm')
   const searchStr = process.argv[2]
   console.log("searching ... ", searchStr)
   await page.keyboard.type(searchStr, { delay: 200 })
-  const search_btn = await page.waitForSelector('#space6 > li.box_flex > button', { timeout: 1000 }) // 검색 클릭
+
+  // 검색
+  const search_btn = await page.waitForSelector('#space7 > li.box_flex > button', { timeout: 1000 }) // 검색 클릭
   search_btn?.click()
   console.log("start location searching button clicked...")
-  // 출발지  input box click
-  const startInputCheck = await page.waitForSelector('#selODStartArea_1', { timeout: 10000 })
-  startInputCheck?.click()
-  await page.waitForTimeout(1000)
-  // 도착지 전체선택
-  const endInputBox = await page.waitForSelector('#rdAlocSelN', { timeout: 10000 })
-  endInputBox?.click()
+  // 검색 결과 input check
+  const destinationInputCheck = await page.waitForSelector('#selODEndArea_1', { timeout: 1000 })
+  destinationInputCheck?.click()
 
   // 결과 검색
   const result_btn = await page.waitForSelector('#btnSearch > button', { timeout: 1000 }) // 검색 클릭
   result_btn?.click()
-  //download
+  // download
   await page.waitForSelector('#none', { timeout: 1000 })
   const domBtn = await page.waitForSelector('#rgrstyReportResult > h2 > p > span:nth-child(1)', { timeout: 10000 })
   domBtn?.click()
-  // screenshot
+  // back to selection page
   const backBtn = await page.waitForSelector('#tab1 > div.pivotResult > div.but_area_back > button', { timeout: 10000 })
   backBtn?.click()
-
   await page.waitForTimeout(100000)
   // await page.screenshot({ path: "./stcis.png", fullPage: true })
   console.log("browser closing ...")
   await browser.close()
-} 
+}
