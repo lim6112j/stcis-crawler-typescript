1ff5e1a462ad081ffbd083d749c95b46904bd326
ben lim <lim6112j@gmail.com>
2023-05-19 17:14:15 +0900

    동 검색

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import { Page } from 'puppeteer'
+import puppeteer, { Page } from 'puppeteer'
 import { Cluster } from 'puppeteer-cluster'
 import mssd from './multiStartSingleDestination'
 import ssmd from './singleStartMultiDestination'
+import getDong from './searchDong'
 async function download(page: Page, searchStr: string, type: number) {
   switch (type) {
     case 0:
@@ -15,12 +16,15 @@ async function download(page: Page, searchStr: string, type: number) {
 }
 (async () => {
 
-  // try {
-  //   if (process.argv[2] === undefined) { throw new Error("argument needed") }
-  // } catch (e) {
-  //   console.log('argument needed')
-  //   process.exit(1)
-  // }
+  try {
+    if (process.argv[2] === undefined) { throw new Error("argument needed") }
+  } catch (e) {
+    console.log('argument needed')
+    process.exit(1)
+  }
+  const searchStr = process.argv[2]
+  const dongs = await getDong('아산시')
+  console.log(dongs)
   const cluster = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_BROWSER,
     maxConcurrency: 1,
@@ -28,14 +32,14 @@ async function download(page: Page, searchStr: string, type: number) {
       headless: false
     }
   });
-  await cluster.task(async ({ page, data }) => {
-    await download(page, data.searchStr, data.type)
-  })
-  const listOfSearch = ['아산시', '경주시', '안산시']
-  listOfSearch.forEach(val => {
-    cluster.queue({ searchStr: val, type: 0 })
-    cluster.queue({ searchStr: val, type: 1 })
-  })
+  // await cluster.task(async ({ page, data }) => {
+  //   await download(page, data.searchStr, data.type)
+  // })
+  // const listOfSearch = ['아산시', '경주시', '안산시']
+  // listOfSearch.forEach(val => {
+  //   cluster.queue({ searchStr: val, type: 0 })
+  //   cluster.queue({ searchStr: val, type: 1 })
+  // })
   await cluster.idle()
   await cluster.close()
 }
---
+++ searchDong.ts
@@ -0,0 +1,40 @@
+import puppeteer from 'puppeteer'
+const TIMEOUT = 15000
+export default async function getDong(searchStr: string) {
+
+  const browser = await puppeteer.launch({ headless: true })
+  const page = await browser.newPage()
+  await page.goto('http://stcis.go.kr')
+  await page.waitForSelector('body > header > div.header_inner > div.gnb > ul > li:nth-child(1) > a', { timeout: TIMEOUT })
+  await page.focus('body > header > div.header_inner > div.gnb > ul > li:nth-child(1) > a')
+  // main 페이지에서 메뉴 hover 시 서브메뉴 펼처짐(일반버스 도시철도 이용 O/D)
+  await page.waitForSelector('#submenu1 > ul > li:nth-child(4) > ul > li:nth-child(1) > a', { timeout: TIMEOUT })
+  await page.$eval('#submenu1 > ul > li:nth-child(4) > ul > li:nth-child(1) > a', elem => elem.click())
+
+  // 출발지 선택
+  const startInputBox = await page.waitForSelector('#rdStgptSelY', { timeout: TIMEOUT })
+  startInputBox?.click()
+
+  await page.waitForTimeout(1000)
+  await page.waitForSelector('#searchODStartSpaceNm', { timeout: TIMEOUT })
+  await page.focus('#searchODStartSpaceNm')
+  console.log("searching ... ", searchStr)
+  // 출발지  input box click
+  await page.waitForTimeout(100)
+  await page.keyboard.type(searchStr, { delay: 300 })
+
+  await page.waitForTimeout(1000)
+  const search_btn = await page.waitForSelector('#space6 > li.box_flex > button', { timeout: TIMEOUT }) // 검색 클릭
+  await search_btn?.click()
+  console.log("start location searching button clicked...")
+  // 출발지  input box click
+  await page.waitForTimeout(1000)
+  const startInputCheck = await page.waitForSelector('#divODStart > ol > li', { timeout: TIMEOUT })
+  await startInputCheck?.click()
+  await page.waitForTimeout(1000)
+  await page.waitForSelector('#divRdoODAear > li.searchODAreaGubun.active')
+
+  const dongList = await page.evaluate(() => Array.from(document.querySelectorAll('#searchStgptZoneEmd option')).map((element: any) => element.innerText))
+  await browser.close()
+  return dongList
+}
